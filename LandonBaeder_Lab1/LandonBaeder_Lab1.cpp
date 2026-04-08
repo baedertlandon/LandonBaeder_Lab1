5933d259909f8c1b1645b0d566673956236e0995
kthorn11 <kaitmarie713@gmail.com>
2026-04-08 10:05:39 -0500

    Added output line by Kaitlyn Thornton

--- LandonBaeder_Lab1/LandonBaeder_Lab1.cpp
+++ LandonBaeder_Lab1/LandonBaeder_Lab1.cpp
@@ -6,4 +6,5 @@ int main()
 {
     cout << "Hello from Landon! This is my first Git commit." << endl;
     cout << "Git makes collaboration easy!" << endl;
+    cout << "Changes made by Kaitlyn Thornton!" << endl;
 }
